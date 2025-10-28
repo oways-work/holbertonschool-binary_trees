a72431cfa122bee9ec9056414517258f747c2649
oways-work <owayswork@gmail.com>
2025-10-28 18:04:08 +0300

    Add binary_tree_postorder function
    Implement post-order traversal for binary tree.

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Goes through a binary tree using post-order traversal
+ *
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node.
+ * The value in the node must be passed as a parameter to this function.
+ *
+ * Description: If @tree or @func is NULL, do nothing.
+ * Traversal order: Left -> Right -> Root
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	/* 1. Traverse the left subtree */
+	binary_tree_postorder(tree->left, func);
+
+	/* 2. Traverse the right subtree */
+	binary_tree_postorder(tree->right, func);
+
+	/* 3. Visit the root node */
+	func(tree->n);
+}

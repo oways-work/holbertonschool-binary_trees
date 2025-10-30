c1af72ec429e80393f9eed09f0e7ee48713d0e12
oways-work <owayswork@gmail.com>
2025-10-30 14:59:12 +0300

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - Traverses a binary tree using post-order.
+ * binary_tree_postorder - Traverses a binary tree using post
  *
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node.
@@ -13,7 +13,7 @@
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
-	return;
+		return;
 
 	/* 1. Traverse the left subtree */
 	binary_tree_postorder(tree->left, func);

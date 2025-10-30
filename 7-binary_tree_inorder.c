33b9e2693c4ec3e4b1335a0d3af3b37b92c78435
oways-work <owayswork@gmail.com>
2025-10-30 14:55:09 +0300

    Rename inorder function to postorder and update logic

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,27 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - Goes through a binary tree using in-order travse
+ * binary_tree_postorder - Traverses a binary tree using post-order.
  *
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node.
  * The value in the node must be passed as a parameter to this function.
  *
  * Description: If @tree or @func is NULL, do nothing.
- * Traversal order: Left -> Root -> Right
+ * Traversal order: Left -> Right -> Root
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
 
 	/* 1. Traverse the left subtree */
-	binary_tree_inorder(tree->left, func);
+	binary_tree_postorder(tree->left, func);
 
-	/* 2. Visit the root node */
+	/* 2. Traverse the right subtree */
+	binary_tree_postorder(tree->right, func);
+
+	/* 3. Visit the root node */
 	func(tree->n);
-
-	/* 3. Traverse the right subtree */
-	binary_tree_inorder(tree->right, func);
 }
 
